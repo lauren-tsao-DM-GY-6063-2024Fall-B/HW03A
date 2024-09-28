b290925894676b8e62b6802bfadca4233662ecd8
Lauren Tsao <et2676@nyu.edu>
2024-09-28 19:09:43 -0400

    Tidied up code

--- sketch.js
+++ sketch.js
@@ -1,58 +1,55 @@
-function preload(){
-  img01 = loadImage("/images/kusama-00.jpg")
+function preload() {
+  img01 = loadImage("/images/kusama-00.jpg");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(236, 215, 0)
+  background(236, 215, 0);
 
   // image(img01, 0, 0, 400, 500);
 
+  let trans = 255;
 
-let trans = 255
-
-// Multiples of row 1a's
+  // Multiples of row 1a's
   for (let y = 0; y < height; y += 56) {
     // row 1a (big circles)
     for (let x = 5; x < width; x += 56) {
-      strokeWeight(0)
-      fill(21, 24, 31, trans)
+      strokeWeight(0);
+      fill(21, 24, 31, trans);
       ellipse(x, y, 16);
     }
   }
 
-// Multiples of row 1b's
-for (let y = 28; y < height; y += 56) {
-  // row 1b (small circles)
-  for (let x = 5; x < width; x += 56) {
-    strokeWeight(0)
-    fill(21, 24, 31, trans)
-    ellipse(x, y, 8);
+  // Multiples of row 1b's
+  for (let y = 28; y < height; y += 56) {
+    // row 1b (small circles)
+    for (let x = 5; x < width; x += 56) {
+      strokeWeight(0);
+      fill(21, 24, 31, trans);
+      ellipse(x, y, 8);
+    }
   }
-}
 
-// column of row 2a's
+  // column of row 2a's
   for (let y = 28; y < height; y += 56) {
     // row 2a (big circles)
     for (let x = 34; x < width; x += 56) {
-      strokeWeight(0)
-      fill(21, 24, 31, trans)
+      strokeWeight(0);
+      fill(21, 24, 31, trans);
       ellipse(x, y, 16);
     }
   }
 
-// column of row 2b's
-for (let y = 0; y < height; y += 56) {
-  // row 2b (small circles)
-  for (let x = 34; x < width; x += 56) {
-    strokeWeight(0)
-    fill(21, 24, 31, trans)
-    ellipse(x, y, 8);
+  // column of row 2b's
+  for (let y = 0; y < height; y += 56) {
+    // row 2b (small circles)
+    for (let x = 34; x < width; x += 56) {
+      strokeWeight(0);
+      fill(21, 24, 31, trans);
+      ellipse(x, y, 8);
+    }
   }
 }
 
-}
-
 //circle color is 21, 24, 31
-function draw() {
-}
+function draw() {}
